8c6d3df4d213a89824d647fac1c801869d0563be
chaitra <72888497+Chaitra001-smartcoder@users.noreply.github.com>
2021-01-02 13:22:51 +0530

    init

---
+++ src/app/mainblog/mainblog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-mainblog',
+  templateUrl: './mainblog.component.html',
+  styleUrls: ['./mainblog.component.css']
+})
+export class MainblogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
